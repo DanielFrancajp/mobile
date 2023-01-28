b56cc4063c4073f6a83d92854743f32cbde2b518
DanielFrancajp <danieldenotas@gmail.com>
2023-01-28 12:43:32 -0300

    Finalização do checkbox + tela New

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -3,9 +3,13 @@ import {
     View,
     ScrollView,
     Text,
-    TextInput
+    TextInput,
+    TouchableOpacity
 
 } from 'react-native'
+
+import { Feather } from '@expo/vector-icons'
+import colors from 'tailwindcss/colors'
 import { BackButton } from '../components/BackButton'
 import { CheckBox } from '../components/CheckBox'
 
@@ -36,7 +40,10 @@ export function New() {
     return (
         <View className='flex-1 bg-background px-8 pt-16'>
 
-            <ScrollView showsVerticalScrollIndicator={false}>
+            <ScrollView showsVerticalScrollIndicator={false}
+            contentContainerStyle={{paddingBottom: 100}} 
+            >
+            
                 <BackButton />
 
                 <Text className='mt-6 text-white font-extrabold text-3xl'>
@@ -49,11 +56,14 @@ export function New() {
 
                 <TextInput
                     className='h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600'
+                    placeholder='Estudar, dormir , Trabalhar etc...'
+                    placeholderTextColor={colors.zinc[400]}
                 />
 
                 <Text className='font-semibold mt-4 mb-3 text-white text-base'>
                     Qual a recorrência?
                 </Text>
+
                 {
                     availableWeekDays.map((weekDay, index) => (
                         <CheckBox
@@ -61,9 +71,29 @@ export function New() {
                             title={weekDay}
                             checked={weekDays.includes(index)}
                             onPress={() => handleToggleWeekDay(index)}
+
                         />
+
                     ))
+
                 }
+
+                <TouchableOpacity
+                    className='w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6'
+                    activeOpacity={0.7}
+                >
+                    <Feather
+                        name='check'
+                        size={20}
+                        color={colors.white}
+                    />
+
+                    <Text className='font-semibold text-base text-white ml-2'>
+                        Confirmar
+                    </Text>
+                </TouchableOpacity>
+
+
 
             </ScrollView>
         </View>
